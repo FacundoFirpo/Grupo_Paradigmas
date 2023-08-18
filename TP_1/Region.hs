5e304ec82908048aface402e8322f2221afe9108
FacundoFirpo <ffirpo@udesa.edu.ar>
2023-08-18 14:31:05 -0300

    commit Region

--- TP_1/Region.hs
+++ TP_1/Region.hs
@@ -8,19 +8,19 @@ import Quality
 
 data Region = Reg [City] [Link] [Tunel]
 newR :: Region
-
+newR = Reg -- newR cities links tunels = Reg cities links tunels
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
-
+foundR (Reg cities _ _) = flip (:) cities -- foundR (Reg cities _ _) city = city : cities
 
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
-
+linkR (Reg _ links _) = flip (:) links newL -- linkR (Reg _ links _) city1 city 2 =  newL city1 city2 : links 
 
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
-
+tunelR (Reg _ _ tunels) = flip (:) tunels -- tunelR (Reg _ _ tunels) cities = cities : tunels
 
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
-
+connectedR (Reg _ _ tunels) city1 city2 = elem True (map (connectsT city1 city2 each) tunels)
 
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
 
