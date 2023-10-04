7785ddacb2a276ee796cb5b58c1da7bc8f91ff18
Francisco Carruthers <fcarruthers@udesa.edu.ar>
2023-10-04 12:31:32 -0300

    Creo tests 01 al 05

--- TP_3/src/tp3/Nemo.java
+++ TP_3/src/tp3/Nemo.java
@@ -1,4 +1,44 @@
 package tp3;
 
 public class Nemo {
+    private int depth;
+    private int xPosition;
+    private int yPosition;
+    private int direccion;
+    public Nemo() {
+        depth = 0;
+        xPosition = 0;
+        yPosition = 0;
+        direccion = 0;
+    }
+
+    public boolean isOnSurface() {
+        return depth == 0;
+    }
+
+    public int[] getPosition() {
+        int[] position = {xPosition, yPosition, depth};
+        return position;
+    }
+
+    public void move(String order) {
+        for (int i = 0; i < order.length(); i++) {
+            if (order.charAt(i) == 'd') {
+                depth++;
+            } else if (order.charAt(i) == 'u') {
+                if (depth > 0) {
+                    depth--;
+                }
+            }
+        }
+    }
+    public void moveChar(char order) {
+        if (order == 'd') {
+            depth++;
+        } else if (order == 'u') {
+            if (depth > 0) {
+                depth--;
+            }
+        }
+    }
 }
--- TP_3/src/tp3/NemoTests.java
+++ TP_3/src/tp3/NemoTests.java
@@ -1,4 +1,36 @@
 package tp3;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class NemoTests {
+    @Test
+    public void test01NewNemoIsOnSurface() {
+        Nemo nemo = new Nemo();
+        assertTrue(nemo.isOnSurface());
+    }
+    @Test
+    public void test02NoMovementWhenNoOrderGiven(){
+        Nemo nemo = new Nemo();
+        nemo.move(" ");
+        assertArrayEquals(new int[]{0, 0, 0}, nemo.getPosition());
+    }
+    @Test
+    public void test03NemoMovesDownWhenGivenOrder(){
+        Nemo nemo = new Nemo();
+        nemo.move("dddu");
+        assertArrayEquals(new int[]{0,0,2}, nemo.getPosition());
+    }
+    @Test
+    public void test04NemoMovesWhenGivenCharOrder(){
+        Nemo nemo = new Nemo();
+        nemo.moveChar('d');
+        assertArrayEquals(new int[]{0,0,1}, nemo.getPosition());
+    }
+    @Test
+    public void test05NemoFacesNorthWhenCreated(){
+        Nemo nemo = new Nemo();
+        assertEquals("North", nemo.getdirection());
+    }
 }
